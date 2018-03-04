8a00ea175750890e14d20e2a834e26ea2cc882de
Lang Lin <mikelinlang@gmail.com>
2018-03-04 05:00:51 -0500

    camera
    last efforts

--- app/src/main/java/qreet/qreet/MainActivity.java
+++ app/src/main/java/qreet/qreet/MainActivity.java
@@ -1,13 +1,80 @@
 package qreet.qreet;
 
+import android.content.Context;
+import android.content.Intent;
+
+import android.hardware.Camera;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+import static android.content.ContentValues.TAG;
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
+import static java.sql.DriverManager.println;
 
 public class MainActivity extends AppCompatActivity {
+    public String mCurrentPhotoPath = null;
+    private Camera mcam = null;
+    private SurfaceHolder mHolder;
+
+    public static int REQUEST_TAKE_PHOTO = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+            if (pictureFile == null){
+                Log.d(TAG, "Error creating media file, check storage permissions: " +
+                        e.getMessage());
+                return;
+            }
+
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+            } catch (FileNotFoundException e) {
+                Log.d(TAG, "File not found: " + e.getMessage());
+            } catch (IOException e) {
+                Log.d(TAG, "Error accessing file: " + e.getMessage());
+            }
+        }
+    };
+    Button captureButton = (Button) findViewById(id.button_capture);
+    captureButton.setOnClickListener(
+        new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // get an image from the camera
+            mcam.takePicture(null, null, mPicture);
+        }
+    }
+
+);
+
 }
+
+
